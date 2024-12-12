27740ae799cee1e80053a649a05acbf2dc5560dc
Ankit Rijal <ankitrijal2054@gmail.com>
2024-12-13 04:41:05 +0545

    Updated some somponents alignment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,75 +48,82 @@ function App() {
     <div
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
-      <div style={{ textAlign: "center", flex: 1 }}>
-        <header style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-          <h1 style={{ color: "#4CAF50" }}>Sentiment Analysis</h1>
-        </header>
+      <div style={{ textAlign: "center" }}>
+        <div>
+          <div style={{ textAlign: "center", flex: 1 }}>
+            <header
+              style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}
+            >
+              <h1 style={{ color: "#4CAF50" }}>Sentiment Analysis</h1>
+            </header>
 
-        <textarea
-          rows="5"
-          cols="50"
-          placeholder="Enter text here..."
-          value={inputText}
-          onChange={(e) => onInputTextChange(e.target.value)}
-          style={{
-            padding: "10px",
-            border: "2px solid #4CAF50",
-            borderRadius: "5px",
-            fontSize: "16px",
-            width: "80%",
-          }}
-        />
+            <textarea
+              rows="5"
+              cols="50"
+              placeholder="Enter text here..."
+              value={inputText}
+              onChange={(e) => onInputTextChange(e.target.value)}
+              style={{
+                padding: "10px",
+                border: "2px solid #4CAF50",
+                borderRadius: "5px",
+                fontSize: "16px",
+                width: "80%",
+              }}
+            />
 
-        <br />
+            <br />
 
-        <button
-          onClick={handleSubmit}
-          style={{
-            marginTop: "10px",
-            padding: "10px 20px",
-            backgroundColor: inputText ? "#4CAF50" : "#ccc",
-            color: inputText ? "#fff" : "#666",
-            border: "none",
-            borderRadius: "5px",
-            fontSize: "16px",
-            cursor: inputText ? "pointer" : "not-allowed",
-          }}
-          disabled={!inputText}
-        >
-          Analyze Sentiment
-        </button>
-
-        {result && (
-          <div
-            style={{
-              marginTop: "20px",
-              padding: "20px",
-              border: "2px solid #4CAF50",
-              borderRadius: "10px",
-              display: "inline-block",
-              textAlign: "left",
-              backgroundColor: "#f9f9f9",
-            }}
-          >
-            <h2 style={{ textAlign: "center" }}>
-              Prediction Results {getSentimentEmoji(result.predicted_sentiment)}
-            </h2>
-            <p>
-              <strong>Sentiment:</strong> {result.predicted_sentiment}{" "}
-              {getSentimentEmoji(result.predicted_sentiment)}
-            </p>
-            <p>
-              <strong>Negative:</strong> {(result.negative * 100).toFixed(2)}%
-            </p>
-            <p>
-              <strong>Neutral:</strong> {(result.neutral * 100).toFixed(2)}%
-            </p>
-            <p>
-              <strong>Positive:</strong> {(result.positive * 100).toFixed(2)}%
-            </p>
+            <button
+              onClick={handleSubmit}
+              style={{
+                marginTop: "10px",
+                padding: "10px 20px",
+                backgroundColor: inputText ? "#4CAF50" : "#ccc",
+                color: inputText ? "#fff" : "#666",
+                border: "none",
+                borderRadius: "5px",
+                fontSize: "16px",
+                cursor: inputText ? "pointer" : "not-allowed",
+              }}
+              disabled={!inputText}
+            >
+              Analyze Sentiment
+            </button>
           </div>
-        )}
+
+          {result && (
+            <div
+              style={{
+                marginTop: "20px",
+                padding: "20px",
+                border: "2px solid #4CAF50",
+                borderRadius: "10px",
+                display: "inline-block",
+                textAlign: "left",
+                backgroundColor: "#f9f9f9",
+              }}
+            >
+              <h2 style={{ textAlign: "center" }}>
+                Prediction Results{" "}
+                {getSentimentEmoji(result.predicted_sentiment)}
+              </h2>
+              <p>
+                <strong>Sentiment:</strong> {result.predicted_sentiment}{" "}
+                {getSentimentEmoji(result.predicted_sentiment)}
+              </p>
+              <p>
+                <strong>Negative:</strong> {(result.negative * 100).toFixed(2)}%
+              </p>
+              <p>
+                <strong>Neutral:</strong> {(result.neutral * 100).toFixed(2)}%
+              </p>
+              <p>
+                <strong>Positive:</strong> {(result.positive * 100).toFixed(2)}%
+              </p>
+            </div>
+          )}
+        </div>
       </div>
 
       <footer
