b7ac26bec4e8ba47cf1e1862a768a91eb176a2e2
Ankit Rijal <ankitrijal2054@gmail.com>
2024-12-13 23:58:24 +0545

    Fixed URL issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const handleSubmit = async () => {
     try {
       const response = await fetch(
-        "https://sentiment-analysis-455b.onrender.com",
+        "https://sentiment-analysis-455b.onrender.com/predict",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
